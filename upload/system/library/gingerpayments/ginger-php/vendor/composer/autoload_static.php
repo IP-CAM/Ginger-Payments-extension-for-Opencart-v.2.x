e2dd2c381bb8084e8add244737c341ee6d71b97c
Ruslan Dmitrijev <ruslan@dmitrijev.net>
2017-01-26 13:13:48 +0200

    Updated ginger-php library to 1.2.4 version

--- upload/system/library/gingerpayments/ginger-php/vendor/composer/autoload_static.php
+++ upload/system/library/gingerpayments/ginger-php/vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit4df64a7cc23e5645b267d4c6123b06b3
+class ComposerStaticInit60a617e5c8399950bf77bb152497d027
 {
     public static $files = array (
         'ad155f8f1cf0d418fe49e248db8c661b' => __DIR__ . '/..' . '/react/promise/src/functions_include.php',
@@ -904,10 +904,10 @@ class ComposerStaticInit4df64a7cc23e5645b267d4c6123b06b3
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit4df64a7cc23e5645b267d4c6123b06b3::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit4df64a7cc23e5645b267d4c6123b06b3::$prefixDirsPsr4;
-            $loader->prefixesPsr0 = ComposerStaticInit4df64a7cc23e5645b267d4c6123b06b3::$prefixesPsr0;
-            $loader->classMap = ComposerStaticInit4df64a7cc23e5645b267d4c6123b06b3::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit60a617e5c8399950bf77bb152497d027::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit60a617e5c8399950bf77bb152497d027::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit60a617e5c8399950bf77bb152497d027::$prefixesPsr0;
+            $loader->classMap = ComposerStaticInit60a617e5c8399950bf77bb152497d027::$classMap;
 
         }, null, ClassLoader::class);
     }
